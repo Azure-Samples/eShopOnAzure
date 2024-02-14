e9eefbe245c0d4bb00932e6bd4f46d2e7d844e7d
James Montemagno <james.montemagno@gmail.com>
2024-02-14 08:58:45 -0800

    remove client v2

--- src/HybridApp/Services/ProductImageUrlProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using eShop.WebAppComponents.Services;
-
-namespace eShop.HybridApp.Services;
-
-public class ProductImageUrlProvider : IProductImageUrlProvider
-{
-    public string GetProductImageUrl(int productId)
-        => $"{MauiProgram.MobileBffCatalogBaseUrl}api/v1/catalog/items/{productId}/pic";
-}
